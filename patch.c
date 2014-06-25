c2d2c1a5ffa600efa37f77f07158e88f647e4d41
PythEch <pythech.tr@gmail.com>
2014-06-25 17:18:14 +0300

    Misc

--- patch.c
+++ patch.c
@@ -15,15 +15,17 @@
 #include <stdio.h>
 #include <string.h>
 
-#define patch(addr, str) fseek(handle, addr, SEEK_SET), fwrite(str, sizeof(char), strlen(str), handle)
+#define patch(addr, str) fseek(handle, addr, SEEK_SET), fwrite(str, sizeof(char), sizeof(str)-1, handle)
 
 int main()
 {
     FILE *handle = fopen("Pangu_v1.0.exe", "r+b");
     if (handle == NULL)
     {
-    	perror("Could not open Pangu_v1.0.exe");
-    	return -1;
+        perror("Could not open Pangu_v1.0.exe");
+        printf("Press any key to continue...");
+        getchar();
+        return -1;
     }
 
     int whitespace_patches[41] = {0x00220F5C, 0x00220F86, 0x00220FBD, 0x002210A9, 0x002210C4, 0x0022110D, 0x00221155, 0x0022118D, 0x002211E1, 0x00221237, 0x0022128A, 0x00221304, 0x0022135C, 0x00221568, 0x0022157A, 0x002215E2, 0x0022231A, 0x00222446, 0x002224C1, 0x002224D9, 0x002224FC, 0x0022252C, 0x00222577, 0x002225A4, 0x002225C0, 0x002225E8, 0x00222602, 0x00222640, 0x00222684, 0x002226A1, 0x00222711, 0x0022272D, 0x00222762, 0x002227A6, 0x00222A55, 0x00222A6B, 0x00222AFA, 0x00222B13, 0x00222B22, 0x00222B7E, 0x00222BB2};
@@ -58,6 +60,9 @@ int main()
     fclose(handle);
 
     printf("Done patching Pangu_v1.0.exe! It should be in English now...\n");
+
+    printf("Press any key to continue...");
+    getchar();
 
     return 0;
 }
